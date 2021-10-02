ebeb60829411ac6258ce7fb981f4297fe0390716
vincent <weqqss@yahoo.com>
2021-10-02 11:23:27 +0700

    update refactor with more eficient code

--- refactor.js
+++ refactor.js
@@ -11,30 +11,7 @@
 // }
 
 function findFirstStringInBracket(str) {
-  let result = ''
-  let isBracketFound = false
-
-  if (str.length < 1) {
-    return ''
-  }
-
-  for (let i = 0; i < str.length; i++) {
-    if (str[i] == '(') {
-      isBracketFound = true
-      continue
-    }
-
-    if (str[i] == ')') {
-      isBracketFound = false
-      break
-    }
-
-    if (isBracketFound) {
-      result += str[i]
-    }
-  }
-
-  return result
+  return str.split('(')[1].split(')')[0]
 }
 
 console.log(findFirstStringInBracket('(aku kamu)'))
